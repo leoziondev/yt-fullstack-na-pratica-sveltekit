f9dff1ac8c9afb529bd1c89143421776221aaa5f
leoziondev <leozionsuporte@gmail.com>
2023-08-10 16:10:20 -0300

    layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/sobre">Sobre</a></li>
+    <li><a href="/portfolio">Portfolio</a></li>
+    <li><a href="contato">Contato</a></li>
+</ul>
+
+<slot />

d1983107b8a4f3eb115c11c5238db06040b15102
leoziondev <leozionsuporte@gmail.com>
2023-08-10 16:56:26 -0300

    example component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
+<script>
+    import Button from './Button.svelte'
+  import { fromJSON } from 'postcss';
+</script>
+
 <h1>MyApp</h1>
+<Button />
+<Button label="Not Click me" />
+<Button label="Another button" />
+<Button label="Example Button" />

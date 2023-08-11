341a73cb45b098af0db523f1374be2d875dfee8d
leoziondev <leozionsuporte@gmail.com>
2023-08-11 11:54:46 -0300

    refactor nav component

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,7 +1,21 @@
+<script>
+    const navItems = [
+        { href: '/', label: 'Home' },
+        { href: '/sobre', label: 'Sobre' },
+        { href: '/portfolio', label: 'Portfolio' },
+        { href: '/contato', label: 'Contato' },
+        { href: '/faq', label: 'Perguntas Frequentes' },
+    ]
+</script>
+
 <nav>
-    <a href="/" class="text-gray-800 transition-colors duration-300 transform border-b-2 border-blue-500 mx-1.5 sm:mx-6">Home</a>
-    <a href="/sobre" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform hover:border-blue-500 mx-1.5 sm:mx-6">Sobre</a>
-    <a href="/portfolio" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform hover:border-blue-500 mx-1.5 sm:mx-6">Portfolio</a>
-    <a href="/contato" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform hover:border-blue-500 mx-1.5 sm:mx-6">Contato</a>
-    <a href="/contato" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform hover:border-blue-500 mx-1.5 sm:mx-6">Perguntas Frequentes</a>
+    {#each navItems as {href, label}}
+        <a
+            {href}
+            class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform hover:border-blue-500 mx-1.5 sm:mx-6"
+        >
+            {label}
+        </a>    
+    {/each}
+    <!-- <a href="/" class="text-gray-800 transition-colors duration-300 transform border-b-2 border-blue-500 mx-1.5 sm:mx-6">Home</a> -->    
 </nav>

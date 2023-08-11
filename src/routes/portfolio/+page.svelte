d66f04a25b7fd36d661d12ad34960e3f2f0597f2
leoziondev <leozionsuporte@gmail.com>
2023-08-11 20:15:16 -0300

    filter list data

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,34 +1,45 @@
-<script>
+<script lang="ts">
     export let data
+    let currentCategory = 'All'
+    let portfolioItems = data.items
+
+    function changeCategory(newCategory: string) {
+        currentCategory = newCategory;
+        filterPortfolio()
+    }
+
+    function filterPortfolio() {
+        portfolioItems = data.items.filter(item => {
+            return item.category === currentCategory || currentCategory === 'All';
+        })
+    }
 </script>
 
 <section class="bg-white">
     <div class="container px-6 mx-auto">
 
         <div class="flex py-4 mt-4 overflow-x-auto overflow-y-hidden md:justify-center">
-            <button
-                class="h-12 px-8 py-2 -mb-px text-sm text-center text-blue-600 bg-transparent border-b-2 border-blue-500 sm:text-base dark:border-blue-400 whitespace-nowrap focus:outline-none">
-                Animation
-            </button>
+            {#each data.categories as category}
 
-            <button
-                class="h-12 px-8 py-2 -mb-px text-sm text-center text-gray-700 bg-transparent border-b-2 border-gray-200 sm:text-base cursor-base focus:outline-none hover:border-gray-400">
-                Web design
-            </button>
+                {#if currentCategory === category}
+                    <button
+                        class="h-12 px-8 py-2 -mb-px text-sm text-center text-blue-600 bg-transparent border-b-2 border-blue-500 sm:text-base dark:border-blue-400 whitespace-nowrap focus:outline-none">
+                        {category}
+                    </button>
+                {:else}
+                    <button
+                        on:click={() => changeCategory(category)}
+                        class="h-12 px-8 py-2 -mb-px text-sm text-center text-gray-700 bg-transparent border-b-2 border-gray-200 sm:text-base cursor-base focus:outline-none hover:border-gray-400">
+                        {category}
+                    </button>
+                {/if}
 
-            <button
-                class="h-12 px-8 py-2 -mb-px text-sm text-center text-gray-700 bg-transparent border-b-2 border-gray-200 sm:text-base cursor-base focus:outline-none hover:border-gray-400">
-                App design
-            </button>
-
-            <button
-                class="h-12 px-8 py-2 -mb-px text-sm text-center text-gray-700 bg-transparent border-b-2 border-gray-200 sm:text-base cursor-base focus:outline-none hover:border-gray-400">
-                Branding
-            </button>
+            {/each}
+            
         </div>
 
         <section class="mt-8 lg:mt-12 lg:grid lg:grid-cols-2 xl:grid-cols-4 lg:gap-12">
-            {#each data.items as { title, category, image }}
+            {#each portfolioItems as { title, category, image }}
                 <article class="">
                     <div class="">
                         <p class="text-lg tracking-wider text-blue-500 uppercase">{category}</p>
--- src/routes/portfolio/+page.ts
+++ src/routes/portfolio/+page.ts
@@ -1,5 +1,12 @@
 export function load() {
     return {
+        categories: [
+            'All',
+            'Ui kit',
+            'Wireframe',
+            'Components',
+            'Strategy'
+        ],
         items: [
             {title: 'Block Of UI Kit Collections', category: 'Ui kit', image: 'https://images.unsplash.com/photo-1621609764180-2ca554a9d6f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'},
             {title: 'Best free website layout', category: 'Wireframe', image: 'https://images.unsplash.com/photo-1600132806608-231446b2e7af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80'},

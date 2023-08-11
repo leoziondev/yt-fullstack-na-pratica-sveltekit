b50cb8dda59c35dcd4971577c4e153898c91d7f8
leoziondev <leozionsuporte@gmail.com>
2023-08-11 12:16:08 -0300

    active menu link

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { page } from "$app/stores";
+
     const navItems = [
         { href: '/', label: 'Home' },
         { href: '/sobre', label: 'Sobre' },
@@ -10,12 +12,15 @@
 
 <nav>
     {#each navItems as {href, label}}
-        <a
-            {href}
-            class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform hover:border-blue-500 mx-1.5 sm:mx-6"
-        >
-            {label}
-        </a>    
-    {/each}
-    <!-- <a href="/" class="text-gray-800 transition-colors duration-300 transform border-b-2 border-blue-500 mx-1.5 sm:mx-6">Home</a> -->    
+        {#if $page.url.pathname === href}
+            <a {href} class="text-gray-800 transition-colors duration-300 transform border-b-2 border-blue-500 mx-1.5 sm:mx-6">{label}</a>
+        {:else}
+            <a
+                {href}
+                class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform hover:border-blue-500 mx-1.5 sm:mx-6"
+            >
+                {label}
+            </a>  
+        {/if}  
+    {/each}   
 </nav>
